f63718e9c81246b95d5af8379ff3124b9a832b63
zahrasarmadi <zahrasarmadi17@gmail.com>
2024-01-25 22:15:28 +0330

    fix student ripository

--- HW13-1/Repository/StudentRepository.cs
+++ HW13-1/Repository/StudentRepository.cs
@@ -9,41 +9,31 @@ public class StudentRepository
     public void AddCourse(int courseId)
     {
         Database.students = serializationST.ReadFromFile<Student>();
-        Database.trainingCourses = serializationCS.ReadFromFile<TrainingCourse>();
         var target = Database.trainingCourses.FirstOrDefault(c => c.Id == courseId);
         var targetStudent = Database.students.FirstOrDefault(s => s.Id == Database.OnlineStudent.Id);
-        if (target.Students == null)
-        {
-            target.Students.Add(targetStudent);
-        }
-        else
-        {
-            target.Students.Add(Database.OnlineStudent);
-        }
+
         var studentCourse = new StudentCourse()
         {
             trainingCourse = target
         };
-        if (Database.OnlineStudent.Courses == null)
+        if (targetStudent.Courses == null)
         {
-            Database.OnlineStudent.Courses = new List<StudentCourse> { studentCourse };
+            targetStudent.Courses = new List<StudentCourse> { studentCourse };
         }
         else
         {
-            Database.OnlineStudent.Courses.Add(studentCourse);
+            targetStudent.Courses.Add(studentCourse);
         }
-        serializationCS.SaveToFileWhitWrite(Database.trainingCourses);
         serializationST.SaveToFileWhitWrite(Database.students);
     }
     public List<StudentCourse> GetCouse()
     {
         Database.students = serializationST.ReadFromFile<Student>();
-        var studentCourse = Database.OnlineStudent.Courses;
-        if(studentCourse==null)
+        var studentCourse = Database.students.FirstOrDefault(s => s.Id == Database.OnlineStudent.Id).Courses;
+        if (studentCourse == null)
         {
             return new List<StudentCourse>();
         }
         return studentCourse;
     }
-
 }
